92515537fd555d2a2fcc86be69bcf2acd818860c
Johann <jschroeder89@email.com>
2020-10-22 18:29:57 +0200

    int debug

--- pyuserinput/mouse.py
+++ pyuserinput/mouse.py
@@ -1,7 +1,6 @@
-import serial
 import json
+import serial
 import time
-import os 
 import time
 import numpy as np
 
@@ -11,23 +10,13 @@ ser = serial.Serial('/dev/ttyUSB0', 115200)
 #encode/decode utf8 .split
 while True:
     x = ser.readline().decode('utf8').strip()
-    break;
+    break
 while True:
     x = ser.readline().decode('utf8').strip()
     y = ser.readline().decode('utf8').strip()
     
     j1 = json.loads(x)
-    j2 = json.loads(y)
-
-    if j1[0] == "LEFT_CLICK" and j2[0] == "DOUBLE_CLICK":
-        BNO055_arr = np.array(j1[1])
-        BMI160_arr = np.array(j2[1])
-        print(BNO055_arr, BMI160_arr)
-    else:
-        BNO055_arr = np.array(j2[1])
-        BMI160_arr = np.array(j1[1]) 
-        print(BNO055_arr, BMI160_arr)   
-    
+    j2 = json.loads(y)   
     
     """if j1[0] == "BNO055_0x28" and j2[0] == "BMI160":
         BNO055_arr = np.array(j1[1])
@@ -35,5 +24,4 @@ while True:
     else:
         BNO055_arr = np.array(j2[1])
         BMI160_arr = np.array(j1[1])    
-    
     """
